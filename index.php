5143f078d638656d5e577f2da8405dc6f628491d
rruggiero5 <74377763+rruggiero5@users.noreply.github.com>
2020-11-25 13:43:42 -0300

    Update index.php
    alterando o html

--- index.php
+++ index.php
@@ -1,11 +1,66 @@
-<h1>Bem vindo a aula de CI e CD do treinamento Gama Itau 11A</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-<h5>Mudando agora com o pessoal da turma</h5>
-<h4>
-  <? 
-  $i = 6;
-  $i = $i * 350;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
 
-  echo " O resultado da variável feita em PHP é: $i";
-  ?>
-</h4>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <script type="text/javascript" src="login.js"></script>
+
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-12">
+                <br><br>
+                <h3 style="text-align: center;">***** LOGIN - MÚSICAS *****</h3>
+                <br><br>
+            </div>
+            <br><br><br><br>
+        </div>
+
+        <div class="input-group flex-nowrap">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="addon-wrapping">@</span>
+            </div>
+            <input id="txtemail" type="email" class="form-control" placeholder="Digite seu email" aria-label="Username"
+                aria-describedby="addon-wrapping">
+        </div>
+        <br>
+
+        <div class="input-group flex-nowrap">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="addon-wrapping">Senha</span>
+            </div>
+            <input id="txtsenha" type="password" class="form-control" placeholder="Digite sua senha"
+                aria-label="Username" aria-describedby="addon-wrapping">
+        </div>
+        <br>
+
+        <button onclick="logar()" type="button" class="btn btn-primary">Login</button>
+
+    </div>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
